09f56509dbc1dd6c1b6e9a412740189296a7858f
MaximilianPepper <164947441+MaximilianPepper@users.noreply.github.com>
2024-06-06 16:32:40 +0200

    es 1.13

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -17,12 +17,21 @@ const App = () => {
   ];
 
   const [selected, setSelected] = useState(0);
+  const [points, setPoints] = useState(new Uint8Array(anecdotes.length));
   const getRandomNumber = () => {
     setSelected(Math.round(Math.random() * (anecdotes.length - 1)));
+  };
+
+  const voteSelected = () => {
+    const copy = [...points];
+    copy[selected] += 1;
+    setPoints(copy);
   };
   return (
     <div>
       <p>{anecdotes[selected]}</p>
+      <p>has {points[selected]} votes</p>
+      <Button onClick={voteSelected} text={"vote"} />
       <Button onClick={getRandomNumber} text={"next anectode"} />
     </div>
   );
