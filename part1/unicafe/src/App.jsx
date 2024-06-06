70f52ef7dca8f6cd22b85b383608b38350cd8fe3
MaximilianPepper <164947441+MaximilianPepper@users.noreply.github.com>
2024-06-06 15:24:40 +0200

    es 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -12,10 +12,34 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [average, setAverage] = useState(0);
+  const [positive, setPositive] = useState(0);
 
-  const feed1 = () => setGood(good + 1);
-  const feed2 = () => setNeutral(neutral + 1);
-  const feed3 = () => setBad(bad + 1);
+  const feed1 = () => {
+    setGood(good + 1);
+    const good1 = good + 1;
+    setTotal(good1 + neutral + bad);
+    const total1 = total + 1;
+    setAverage((good1 - bad) / total1);
+    setPositive((good1 / total1) * 100);
+  };
+  const feed2 = () => {
+    setNeutral(neutral + 1);
+    const neutral1 = neutral + 1;
+    setTotal(good + neutral1 + bad);
+    const total1 = total + 1;
+    setAverage((good - bad) / total1);
+    setPositive((good / total1) * 100);
+  };
+  const feed3 = () => {
+    setBad(bad + 1);
+    const bad1 = bad + 1;
+    setTotal(good + bad1 + neutral);
+    const total1 = total + 1;
+    setAverage((good - bad1) / total1);
+    setPositive((good / total1) * 100);
+  };
   return (
     <div>
       <h2>give feedback</h2>
@@ -26,6 +50,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {average}</p>
+      <p>positive {positive}%</p>
     </div>
   );
 };
