422c4029b7d462481257a5422734ba4018061b8f
MaximilianPepper <164947441+MaximilianPepper@users.noreply.github.com>
2024-06-08 13:49:15 +0200

    es 2.16

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,6 +1,14 @@
 import { useState, useEffect } from "react";
 import backend from "./backend";
 
+const Notification = ({ message }) => {
+  if (message === null) {
+    return null;
+  }
+
+  return <div className="message">{message}</div>;
+};
+
 const Names = (props) => {
   return (
     <tr>
@@ -65,6 +73,7 @@ const App = () => {
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [newFilter, setNewFilter] = useState("");
+  const [message, setMessage] = useState(null);
 
   const hook = () => {
     backend.getData().then((response) => setPersons(response));
@@ -92,6 +101,8 @@ const App = () => {
         );
         backend.update(p.id, newp).then(() => {
           setPersons(updatedPersons);
+          setMessage(`Update number of ${newName}`);
+          setTimeout(() => setMessage(null), 5000);
           setNewName("");
           setNewNumber("");
         });
@@ -103,6 +114,8 @@ const App = () => {
     const person = { name: newName, number: newNumber };
     backend.create(person).then((person) => {
       setPersons(persons.concat(person));
+      setMessage(`Added ${newName}`);
+      setTimeout(() => setMessage(null), 5000);
       setNewName("");
       setNewNumber("");
     });
@@ -119,6 +132,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={message} />
       <Filter onChange={filterName} value={newFilter} />
 
       <h2>add a new</h2>
