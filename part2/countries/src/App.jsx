87053d82888b1cb2a5376298e7b05d3d35c28aec
MaximilianPepper <164947441+MaximilianPepper@users.noreply.github.com>
2024-06-08 17:51:05 +0200

    es 2.20 part 2 completed

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -2,9 +2,38 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 const baseUrl = "https://studies.cs.helsinki.fi/restcountries/api/all";
 const searchUrl = "https://studies.cs.helsinki.fi/restcountries/api/name/";
+const weatherUrl = `https://api.weatherapi.com/v1/current.json?key=7edfe25e23534a9a9e482919242505&q=`;
+
+const Weather = (props) => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const weatherData = async (country) => {
+      try {
+        const response = await axios.get(`${weatherUrl}${country}`);
+        setData(response.data);
+      } catch (error) {
+        console.error("Error fetching weather data:", error);
+      }
+    };
+
+    weatherData(props.country);
+  }, [props.country]);
+
+  if (!data) return null;
+  return (
+    <div>
+      <h2>Weather in {data.location.name}</h2>
+      <p>temperature {data.current.temp_c} Celsius</p>
+      <img src={data.current.condition.icon}></img>
+      <p>wind {data.current.wind_kph} k/h</p>
+    </div>
+  );
+};
 
 const Countries = (props) => {
   const countries = props.countries;
+
   if (countries.length === 0) return null;
   else if (countries.length > 10)
     return <p>Too many matches, specify another filter</p>;
@@ -23,6 +52,7 @@ const Countries = (props) => {
     );
   } else if (countries.length === 1) {
     const languages = Object.values(countries[0].languages);
+
     return (
       <div>
         <h1>{countries[0].name.common}</h1>
@@ -35,6 +65,7 @@ const Countries = (props) => {
           ))}
         </ul>
         <p>{countries[0].flag}</p>
+        <Weather country={countries[0].capital} />
       </div>
     );
   }
@@ -65,6 +96,7 @@ function App() {
       setCountries([response.data]);
     });
   };
+
   return (
     <>
       <div>
